fb183ea917b76d7f227d5db05c0f009f7e232a56
Gregmar Cruz <gregr1201@gmail.com>
2020-04-07 11:59:19 -0400

    CSS modifications to compare component (pt2)

--- src/containers/HeaderComponent2.js
+++ src/containers/HeaderComponent2.js
@@ -16,8 +16,7 @@ class Header2 extends Component {
 
     render(){
         return (
-            <div className="jumbo">
-                <img src={piggy3} />
+            <div className="jumbo2">
                 <nav className="transparent">
                     <div className="nav-wrapper" style={{borderBottom: "1px solid"}}>
                         <a href="/" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
@@ -37,38 +36,6 @@ class Header2 extends Component {
                     <li style={{textAlign: "center"}}><Link to={'/'}>Login</Link></li>
                     <li style={{textAlign: "center"}}><Link to={'/'}>Register</Link></li>               
                 </ul>
-                <div className="row" style={{paddingTop: "180px"}}>
-                    <div className="col-12">
-                        <h2 style={{textAlign: "center", fontSize: "60px", fontFamily: "Roboto"}} className="mb-3 flow-text white-text"><b>Welcome to Poof! Auto-Compare!</b></h2>
-                        </div>
-                    <div className="col-12 col-md-12" style={{display: "flex", justifyContent: "center"}}>
-                        <form>
-                            <div style={{display: "flex", justifyContent: "center"}}>
-                            <input className="browser-default search-field" style={{display: "flex", width: "40vw", height: "6vh", marginTop: "20px"}} id="search" type="search" required></input>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="col-12 col-md-12 categories" style={{marginTop: "15px", display: "flex", justifyContent: "center", fontFamily: "Roboto"}}>
-                        <ul style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
-                            <li style={{margin: "10px", padding: "20px", paddingLeft: "37px", paddingRight: "37px", textAlign: "center", borderRadius: "5px", border: "3px solid", color: "white"}}>
-                            <i className="medium material-icons">laptop_mac</i>
-                                <div>Electronics</div>
-                            </li>
-                            <li style={{margin: "10px", padding: "20px", paddingLeft: "44px", paddingRight: "44px", textAlign: "center", borderRadius: "5px", border: "3px solid", color: "white"}}>
-                            <i className="medium material-icons">book</i>
-                                <div>Books</div>
-                            </li>
-                            <li style={{margin: "10px", padding: "20px", textAlign: "center", borderRadius: "5px", border: "3px solid", color: "white"}}>
-                            <i className="medium material-icons">store</i>
-                                <div>Clothes/Apparel</div>
-                            </li>
-                            <li style={{margin: "10px", padding: "20px", paddingLeft: "30px", paddingRight: "30px", textAlign: "center", borderRadius: "5px", border: "3px solid", color: "white"}}>
-                            <i className="medium material-icons">toys</i>
-                                <div>Games/Toys</div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
             </div>
             
         )
